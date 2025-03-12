b9d252012fe92b08ce1d2eedd45c9050e28921d3
christopherchege <chrisshege35@gmail.com>
2025-03-12 17:10:39 +0300

    added javascriptfile

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1=31;
+let num2=2;
+const multiply=num1*num2;
+//generate random integer greater than 0
+const random=Math.floor(Math.random()*10)+1;
+//equation that calculate the remainder that is equal to 4
+let num3 =14;
+let num4=5;
+let mod=num3%num4;
+//last challenge
+let max = Math.max(5, 10, 20, 15);
